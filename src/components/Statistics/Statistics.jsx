bc507f9a33c5bbb7a7262d38497f0d86bae8cb14
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-07-18 10:48:25 +1000

    Cambios en el package.json
    cambios en el package.json y creacion del componente Statisticks

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+function Statistics() {
+    //Fujncion que devuelve una lista de licon datos
+    return (
+        <section className='statistics'>
+            <h2 className='title'>{}</h2>
+        </section>
+    );
+}
+
+Statistics.propTypes = {
+    
+}
+
+export default Statistics;
